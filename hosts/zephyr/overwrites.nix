a59fb63bc312a0611c33925c45a3a2f88b593952
Dima <uzhdimka@gmail.com>
2025-02-25 09:42:45 +0100

    Update overwrites.nix

--- hosts/zephyr/overwrites.nix
+++ hosts/zephyr/overwrites.nix
@@ -4,7 +4,7 @@
   services.polybar.settings = {
     "module/battery".battery = lib.mkForce "BAT0";
     "module/backlight".card = lib.mkForce "intel_backlight";
-    "module/temperature".zone-type = lib.mkForce "acpitz";
+    "module/temperature".hwmon-path = lib.mkForce "/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input";
 
   };
 }
