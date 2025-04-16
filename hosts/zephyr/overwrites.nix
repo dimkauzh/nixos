806f390f795615c96695889dcf778c94781ae61f
Dima <uzhdimka@gmail.com>
2025-04-16 10:51:40 +0200

    Turns out this was an issue on polybar

--- hosts/zephyr/overwrites.nix
+++ hosts/zephyr/overwrites.nix
@@ -4,5 +4,6 @@
   services.polybar.settings = {
     "module/battery".battery = lib.mkForce "BAT0";
     "module/backlight".card = lib.mkForce "intel_backlight";
+    "module/title".label-maxlen = lib.mkForce 45;
   };
 }
